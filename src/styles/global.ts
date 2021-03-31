182c5bf195842a2fe540dc391c6e7a904d157ba9
Vitor Rezende <vitorezende@gmail.com>
2021-03-30 22:29:16 -0400

    Criação do componente Header

---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle `
+  :root {
+    --red: #e52e4d;
+    --blue: #5429DC;
+
+    --blue-ligth: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    
+    --background: #f0f2f5;
+    --shape: #ffffff;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  // font-size: 16px(Desktop);
+  // REM = 1rem = font-size da nossa página
+  // Boa prática para garantir a responsividade
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;  // 14px 
+    }
+  }
+
+  
+  body {
+    background:  var (--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+`
